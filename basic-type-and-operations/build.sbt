c0b3f93948cccaed81992fe455a8472ae0f09c5c
Kento Okamura <kentooooo.1230@gmail.com>
2017-12-14 00:52:55 +0900

    第5章 基本形と演算子 終了

---
+++ basic-type-and-operations/build.sbt
@@ -0,0 +1,6 @@
+name := "basic-type-and-operations"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+        
---
+++ basic-type-and-operations/src/main/scala/help1.scala
@@ -0,0 +1,6 @@
+object help1 {
+
+  println("""Welcome to Ultamix 3000.
+           Type "HELP" for help.""")
+
+}
